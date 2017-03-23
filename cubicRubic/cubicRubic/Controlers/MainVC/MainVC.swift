76fe552a2bf88a2b8c05d4bd07845cc48026c290
Юра <yuretz23@ukr.net>
2017-03-23 17:48:24 +0200

    adding MainVC

---
+++ cubicRubic/cubicRubic/Controlers/MainVC/MainVC.swift
@@ -0,0 +1,35 @@
+//
+//  MainVC.swift
+//  cubicRubic
+//
+//  Created by Юра on 23.03.17.
+//  Copyright © 2017 Юра. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
