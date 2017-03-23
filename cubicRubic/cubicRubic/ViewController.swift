948ee108aa381284d9ca06a8655519765a9896bf
Юра <yuretz23@ukr.net>
2017-03-23 17:18:29 +0200

    Create clear project

---
+++ cubicRubic/cubicRubic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  cubicRubic
+//
+//  Created by Юра on 23.03.17.
+//  Copyright © 2017 Юра. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
